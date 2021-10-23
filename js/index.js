105544b2e294e4568ec6222496c48f52abe9b865
crixxcroxx <cristajesawa@gmail.com>
2021-10-23 10:48:57 +0800

    Add option

--- js/index.js
+++ js/index.js
@@ -1,8 +1,17 @@
-let req = new Request(`../data/name.json`)
+let opt = {
+  method: 'GET',
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  mode: 'cors',
+  cache: 'default'
+};
+
+let req = new Request(`../data/name.json`, opt)
 
 fetch(req)
   .then(res => { return res.json() })
   .then(data => {
-    document.getElementById(`name`).innerText = data.students.three
+    document.getElementById(`name`).innerText = data.students.two
   })
   .catch(err => console.error(err))
