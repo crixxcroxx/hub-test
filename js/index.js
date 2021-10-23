8b465d720d9d38b0aa6307d5908ea6be4d940bda
crixxcroxx <cristajesawa@gmail.com>
2021-10-23 10:32:23 +0800

    Add Request() method

--- js/index.js
+++ js/index.js
@@ -1,7 +1,9 @@
-fetch(`../data/name.json`)
+let req = new Request(`../data/name.json`)
+
+fetch(req)
   .then(res => res.json())
   .then(data => {
     console.log(typeof data)
-    document.getElementById(`name`).innerText = data.one
+    document.getElementById(`name`).innerText = data.two
   })
   .catch(err => console.error(err))
